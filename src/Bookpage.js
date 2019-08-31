a99dc51c462d556ebcfca41dec9c9e41d3a57d3c
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-01 03:57:50 +0530

    added books

--- src/Bookpage.js
+++ src/Bookpage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Card from './Card'
 
 const Bookpage = () =>{
     return(
-        <h1>Hello</h1>
+        <Card />
     )
 }
 
---
+++ src/Card.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+
+const Card = () =>{
+    
+    return(
+        <div>
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
+            
+            <div>
+                <h2>The Alchemist</h2>
+                <p>Click me!</p>
+            </div>
+
+
+            </div>
+
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
+            
+            <div>
+                <h2>Harry Potter and the prison of azkaban</h2>
+                <p>Click me!</p>
+            </div>
+
+
+            </div>
+
+
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
+            
+            <div>
+                <h2>The monk who sold his ferrai.</h2>
+                <p>Click me!</p>
+            </div>
+
+
+            </div>
+
+
+
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
+            
+            <div>
+                <h2>The autobiography of a Yogi.</h2>
+                <p>Click me!</p>
+            </div>
+
+
+            </div>
+
+
+
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
+            
+            <div>
+                <h2>Becoming Steve jobs</h2>
+                <p>Click me!</p>
+            </div>
+
+
+            </div>
+
+
+
+        </div>
+
+    );
+}
+
+export default Card;
+
+
+
+
